5e81973c6a6b48eb82c9700c03f94917adfa12a5
zainahmed2382 <zain2407f@aptechsite.net>
2026-07-29 16:26:55 -0700

    .

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,11 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { createRequire } from "module";
-import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 
-const app = require(path.join(__dirname, "..", "dist", "server.cjs")).default;
+const app = require("./_server.cjs").default;
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   await app(req, res);
